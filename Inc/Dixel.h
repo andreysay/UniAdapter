7a2ab917d31d8f5e60f680ded618a68661afc41a
Andrey Spitsyn <andreysay@live.com>
2017-12-01 09:58:41 +0300

    Added Dixel controller initial files.

---
+++ Inc/Dixel.h
@@ -0,0 +1,21 @@
+/**
+  ******************************************************************************
+  * File Name          : Dixel.h
+  * Description        : This file contains the defines of the Modbus protocol
+  ******************************************************************************
+  */
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __DIXEL_H
+#define __DIXEL_H
+
+/* Includes ------------------------------------------------------------------*/
+#include <stdint.h>
+#include <stdbool.h>
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Private define ------------------------------------------------------------*/
+
+#endif /* __DIXEL_H */
+/************************ (C) COPYRIGHT *****END OF FILE****/
